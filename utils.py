8a5d708138646d676c6e78e821767936af162c13
Meghana.E <40365198+Meghana122@users.noreply.github.com>
2019-08-04 18:57:46 +0530

    Delete utils.py

--- utils.py
+++
@@ -1,8 +0,0 @@
-
-import os
-
-def path(filename):
-    filepath = os.path.realpath(__file__)
-    dirpath = os.path.dirname(filepath)
-    fullpath = os.path.join(dirpath, filename)
-    return fullpath
